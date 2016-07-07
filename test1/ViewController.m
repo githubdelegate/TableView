48dae7bf110c33d790323a93526dc4783df7ee2e
zy <ostmail@163.com>
2016-07-07 13:47:28 +0800

    add image

--- test1/ViewController.m
+++ test1/ViewController.m
@@ -42,7 +42,8 @@ const CGFloat NYTopViewH = 244;
     imageView.backgroundColor = [UIColor redColor];
     imageView.contentMode = UIViewContentModeScaleAspectFill;
 //    imageView.image = [UIImage imageNamed:@"60"];
-    imageView.image = [ViewController coreBlurImage:[UIImage imageNamed:@"60"] withBlurNumber:10];
+    UIImage *newImage =  [ViewController imageWithImage:[UIImage imageNamed:@"60"] scaledToSizeWithSameAspectRatio:CGSizeMake(500, 300)];
+    imageView.image = [ViewController coreBlurImage:newImage withBlurNumber:10];
 //    self.tb.tableHeaderView = imageView;
     [self.tb insertSubview:imageView atIndex:0];
     self.imageView = imageView;
@@ -91,6 +92,96 @@ const CGFloat NYTopViewH = 244;
     rect.origin.y = self.tb.contentOffset.y;
     self.imageView.frame = rect;
     
+}
+
++ (UIImage*)imageWithImage:(UIImage*)sourceImage scaledToSizeWithSameAspectRatio:(CGSize)targetSize;
+{
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = targetSize.width;
+    CGFloat targetHeight = targetSize.height;
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    CGPoint thumbnailPoint = CGPointMake(0.0,0.0);
+    
+    if (CGSizeEqualToSize(imageSize, targetSize) == NO) {
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        if (widthFactor > heightFactor) {
+            scaleFactor = widthFactor; // scale to fit height
+        }
+        else {
+            scaleFactor = heightFactor; // scale to fit width
+        }
+        
+        scaledWidth  = width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        
+        // center the image
+        if (widthFactor > heightFactor) {
+            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+        }
+        else if (widthFactor < heightFactor) {
+            thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+        }
+    }
+    
+    CGImageRef imageRef = [sourceImage CGImage];
+    CGBitmapInfo bitmapInfo = CGImageGetBitmapInfo(imageRef);
+    CGColorSpaceRef colorSpaceInfo = CGImageGetColorSpace(imageRef);
+    
+    if (bitmapInfo == kCGImageAlphaNone) {
+        bitmapInfo = kCGImageAlphaNoneSkipLast;
+    }
+    
+    CGContextRef bitmap;
+    
+    if (sourceImage.imageOrientation == UIImageOrientationUp || sourceImage.imageOrientation == UIImageOrientationDown) {
+        bitmap = CGBitmapContextCreate(NULL, targetWidth, targetHeight, CGImageGetBitsPerComponent(imageRef), CGImageGetBytesPerRow(imageRef), colorSpaceInfo, bitmapInfo);
+        
+    } else {
+        bitmap = CGBitmapContextCreate(NULL, targetHeight, targetWidth, CGImageGetBitsPerComponent(imageRef), CGImageGetBytesPerRow(imageRef), colorSpaceInfo, bitmapInfo);
+        
+    }
+    
+    // In the right or left cases, we need to switch scaledWidth and scaledHeight,
+    // and also the thumbnail point
+    if (sourceImage.imageOrientation == UIImageOrientationLeft) {
+        thumbnailPoint = CGPointMake(thumbnailPoint.y, thumbnailPoint.x);
+        CGFloat oldScaledWidth = scaledWidth;
+        scaledWidth = scaledHeight;
+        scaledHeight = oldScaledWidth;
+        
+        CGContextRotateCTM (bitmap, M_PI_2); // + 90 degrees
+        CGContextTranslateCTM (bitmap, 0, -targetHeight);
+        
+    } else if (sourceImage.imageOrientation == UIImageOrientationRight) {
+        thumbnailPoint = CGPointMake(thumbnailPoint.y, thumbnailPoint.x);
+        CGFloat oldScaledWidth = scaledWidth;
+        scaledWidth = scaledHeight;
+        scaledHeight = oldScaledWidth;
+        
+        CGContextRotateCTM (bitmap, -M_PI_2); // - 90 degrees
+        CGContextTranslateCTM (bitmap, -targetWidth, 0);
+        
+    } else if (sourceImage.imageOrientation == UIImageOrientationUp) {
+        // NOTHING
+    } else if (sourceImage.imageOrientation == UIImageOrientationDown) {
+        CGContextTranslateCTM (bitmap, targetWidth, targetHeight);
+        CGContextRotateCTM (bitmap, -M_PI); // - 180 degrees
+    }
+    
+    CGContextDrawImage(bitmap, CGRectMake(thumbnailPoint.x, thumbnailPoint.y, scaledWidth, scaledHeight), imageRef);
+    CGImageRef ref = CGBitmapContextCreateImage(bitmap);
+    UIImage* newImage = [UIImage imageWithCGImage:ref];
+    
+    CGContextRelease(bitmap);
+    CGImageRelease(ref);
+    
+    return newImage; 
 }
 
 
